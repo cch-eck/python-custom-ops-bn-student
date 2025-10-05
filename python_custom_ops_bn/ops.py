fb91cdd8333b2637a55f5b595d3791204a459703
정채현 <88320126+cch-eck@users.noreply.github.com>
2025-10-05 09:34:56 +0900

    Update ops.py

--- python_custom_ops_bn/ops.py
+++ python_custom_ops_bn/ops.py
@@ -15,9 +15,37 @@ def batchnorm_forward(
     eps: float
 ) -> Tuple[Tensor, Tensor, Tensor]:
     """forward pass of BatchNorm for 4D input [N, C, H, W]."""
+    # N: batch size
+    # C: channel size
+    # H: height
+    # W: width
 
     # Implement Here
+    N, C, H, W = input.shape
+    m = N * H * W
+    input = input.permute(1, 0, 2, 3)  # [C, N, H, W]
+    if training:
+        # 1. batch에 대해서 mean, var 계산
+        mean = input.mean(dim=(1, 2, 3))  # [C]
+        var = input.var(dim=(1, 2, 3), unbiased=False)  # [C]
+        invstd = (var + eps).rsqrt()
+        # 2. normalize
+        running_mean.mul_(momentum).add_(mean * (1 - momentum))
+        running_var.mul_(momentum).add_(var * (1 - momentum))
+    else:
+        mean = running_mean
+        invstd = (running_var + eps).rsqrt()
     
+    # 3. scale 
+    xhat = (input - mean[:, None, None, None]) * invstd[:, None, None, None]
+    # and shift
+    output = gamma[:, None, None, None] * xhat + beta[:, None, None, None]
+    
+    # 4. reshape back to [N, C, H, W]
+    output = output.permute(1, 0, 2, 3).contiguous()  # [N, C, H, W]
+    
+    save_mean = mean
+    save_invstd = invstd
     return output, save_mean, save_invstd
 
 
@@ -32,7 +60,24 @@ def batchnorm_backward(
     """backward pass of BatchNorm for 4D input."""
 
     # Implement Here
+    assert grad_output.shape == input.shape
+    assert input.dim() == 4
+    N, C, H, W = input.shape
+    m = N * H * W             # number of elements per channel
+    input = input.permute(1, 0, 2, 3)  # [C, N, H, W]
+    grad_output = grad_output.permute(1, 0, 2, 3)  
+    
+    xhat = (input - save_mean[:, None, None, None]) * save_invstd[:, None, None, None]
+    grad_input = (1. / m) * gamma[:, None, None, None] * save_invstd[:, None, None, None] * (
+        m * grad_output - 
+        grad_output.sum(dim=(1, 2, 3), keepdim=True) - 
+        xhat * (grad_output * xhat).sum(dim=(1, 2, 3), keepdim=True)
+    )
+    grad_gamma = (grad_output * xhat).sum(dim=(1, 2, 3))  # [C]
+    grad_beta = grad_output.sum(dim=(1, 2, 3))          # [C]
 
+    # permute back to [N, C, H, W]
+    grad_input = grad_input.permute(1, 0, 2, 3).contiguous()  # [N, C, H, W]
     return grad_input, grad_gamma, grad_beta
 
 
